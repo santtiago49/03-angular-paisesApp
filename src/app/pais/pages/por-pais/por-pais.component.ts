498c2309a6dd0ca65bf9b24c78f8d21a951d539b
Santiago <santtiago49@gmail.com>
2022-01-05 08:33:03 -0300

    Servicio para buscar paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -8,9 +10,12 @@ export class PorPaisComponent {
 
   termino: string = '';
 
-  constructor() { }
+  constructor( private paisService: PaisService) { }
 
   buscar(){
     console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe( resp => {
+      console.log(resp);
+    })
   }
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl = 'https://restcountries.com/v3.1/'
+
+
+  constructor( private http: HttpClient) { }
+
+  buscarPais( termino: string ): Observable<any> {
+
+    const url = `${ this.apiUrl }/name/${ termino }`;
+
+    return this.http.get(url);
+  }
+  
+}
