a77511a5e768a7dd53d997d937396b897a19aefd
Santiago <santtiago49@gmail.com>
2022-01-06 08:19:20 -0300

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,13 +9,22 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor( private paisService: PaisService) { }
 
   buscar(){
+    this.hayError = false;
+
     console.log(this.termino);
-    this.paisService.buscarPais(this.termino).subscribe( resp => {
-      console.log(resp);
-    })
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe({
+        next: (resp) => { console.log(resp)},
+        error: (err) => { 
+          this.hayError = true;
+          console.info(err)
+        }
+      })  
   }
 }
