b9390f32aef1c32d1727ff4d2f566311ce8b4767
Santiago <santtiago49@gmail.com>
2022-01-22 09:51:06 -0300

    por region, [class], aplicando CSS condicional

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button{
+      margin-right: 5px;
+      margin-bottom: 7px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU',
+                     'AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
+
+  regionActiva: string = ''
+
+  getClassCSS( region: string ): string {
+    return (region === this.regionActiva ) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  cambiarRegion( region: string) {
+    this.regionActiva = region;
   }
+
+
+  // TODO: pasar al servicio la region activa para luego mostrar en tabla 
 
 }
