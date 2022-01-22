cbd8b4ba2e469cbc2889619635d5ee5409cb04e7
Santiago <santtiago49@gmail.com>
2022-01-22 11:20:16 -0300

    mostrar paises por region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,33 +1,43 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interface/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [`
     button{
-      margin-right: 5px;
+      margin-right: 7px;
       margin-bottom: 7px;
     }
   `]
 })
+
+
 export class PorRegionComponent {
 
-  regiones: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU',
-                     'AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
-
+  region: string = ''
   regionActiva: string = ''
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+
+  paises: Country[] = []
 
   getClassCSS( region: string ): string {
     return (region === this.regionActiva ) ? 'btn btn-primary' : 'btn btn-outline-primary'
   }
 
-  constructor() { }
+  constructor( private paisService: PaisService) {}
 
   cambiarRegion( region: string) {
+
+    if( region === this.regionActiva) {return;}
+
+    this.region = region;
+    this.paises = []
     this.regionActiva = region;
+
+    this.paisService.buscarRegion(region)
+      .subscribe( paises => this.paises = paises)
   }
-
-
-  // TODO: pasar al servicio la region activa para luego mostrar en tabla 
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -34,4 +34,10 @@ export class PaisService {
     return this.http.get<Country[]>(url);  
   }
 
+  buscarRegion(region: string): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`;
+
+    return this.http.get<Country[]>(url);  
+  }
+
 }
